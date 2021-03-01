edee55d73a1b5b0e8897072e67f28cc8ed7969bd
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-03-01 12:37:38 +0100

    Use insertCSS ✨

--- background.js
+++ background.js
@@ -2,24 +2,11 @@ function applyConfig(config) {
     chrome.tabs.query({url:'https://web.whatsapp.com/*'}, tabs => {
         // tabs is empty when whatsapp not open.
         for (const tab of tabs) {
-            const port = chrome.tabs.connect(tab.id);
-            port.postMessage(config); // send new config to tab
-            chrome.tabs.sendMessage(tab.id, config);
-            console.log('New settings sent to content script ✓');
-
-            // console.log('Running insertCSS...')
-            // chrome.tabs.insertCSS(tab.id, {
-            //     code: `#pane-side { display: ${config.hide_chats ? 'none' : 'inline'}`
-            // })
-            // console.log('WhatsApp tab is open, but could not communicate -');
-            // console.log('with content script. Inserting it manually...');
-
-            // chrome.tabs.executeScript(tab.id, {
-            //     file: 'content-script.js',
-            // });
-            // port.onMessage.addListener((msg) => {
-            //     console.log('msg', msg);
-            // })
+            console.log('Running insertCSS...')
+            const display = config.hide_chats ? 'none' : 'inline';
+            chrome.tabs.insertCSS(tab.id, {
+                code: `#pane-side { display: ${display} }`
+            })
         }
     });
 }
@@ -34,3 +21,9 @@ chrome.storage.onChanged.addListener(changes => {
 chrome.storage.sync.get({
     hide_chats: true
 }, applyConfig);
+
+// chrome.management.onDisabled.addListener(() => console.log('Focus for whatsapp disabled!!!'));
+
+chrome.runtime.onSuspend.addListener(() => {
+    localStorage.setItem('duderino!!!!', 'giezer');
+})
