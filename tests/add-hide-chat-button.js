fc7c0504b1de213e8be9845a498d0aecb7c89450
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-03-05 14:41:41 +0100

    Add button when context menu triggered

--- tests/add-hide-chat-button.js
+++ tests/add-hide-chat-button.js
@@ -11,16 +11,46 @@ const path = require('path');
   const url = path.resolve('./webpages/with-chat-popup.html');
   await page.goto(`file://${url}`);
 
-  await page.$eval('div[style*="transform-origin: left top;"] ul', ul => {
-    const li = ul.firstElementChild.cloneNode(true);
-    for (let div of li.children) {
-      if (div.getAttribute('role') === 'button' && div.textContent) {
-        div.textContent = 'Hide chat';
-        div.setAttribute('aria-label', 'Hide chat');
+
+
+  // Adding button to context menu functionality.
+  await page.$eval(
+    'div[style*="transform-origin: left top;"] > ul',
+    ul => {
+    // add listener to <span> that always remains in DOM
+    const observer = new MutationObserver((mutationsList, observer) => {
+      console.log('MutationObserver triggered: ', mutationsList);
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length) {
+          console.log('adding hide chat button...');
+          // add context menu.
+          const ul = mutation.addedNodes[0].firstElementChild;
+          console.log('found <ul>', ul);
+          const li = ul.firstElementChild.cloneNode(true);
+          for (const div of li.children) {
+            if (div.getAttribute('role') === 'button' && div.textContent) {
+              div.textContent = 'Hide chat';
+              div.setAttribute('aria-label', 'Hide chat');
+            }
+          }
+          ul.append(li)
+        }
       }
-    }
-    ul.append(li)
+    })
+
+    const div = ul.parentElement;
+    const span = div.parentElement;
+    observer.observe(span, { childList: true, subtree: true });
+
+    // remove div
+    const divNode = div.cloneNode(true);
+    span.removeChild(div);
+    span.appendChild(divNode);
   });
 
+
+
+
+  console.log('Closing browser...');
   await browser.close();
 })();
